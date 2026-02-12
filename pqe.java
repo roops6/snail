0ece3e30120b8dd9780ec65a9185dc9d7fa58535
roops6 <sroopashree15@gmail.com>
2026-02-12 12:07:28 +0530

    Update pqe.java

--- pqe.java
+++ pqe.java
@@ -1,7 +1,5 @@
-class pqe
-  {
-    public static void main(String args[])
-    {
-      System.out.println("hello");
+class pqe{
+public static void main(String args[]){
+    System.out.println("hello");
     }
   }
